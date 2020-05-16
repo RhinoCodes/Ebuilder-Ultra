2b483ed496633c89da7f7a2e4ccc69017d9eb4ee
RhinoCodes <51250021+RhinoCodes@users.noreply.github.com>
2020-05-16 16:39:04 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,7 @@
 import pathlib
 from setuptools import setup
 
-# The directory containing this file
-HERE = pathlib.Path(__file__).parent
-
-# The text of the README file
-README = (HERE / "README.md").read_text()
+README = ("/home/runner/README.md").read_text()
 
 # This call to setup() does all the work
 setup(
