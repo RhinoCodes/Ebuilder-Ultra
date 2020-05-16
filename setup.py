35c6d4f703e2c2603e3081e50813e47c4aab3c64
RhinoCodes <51250021+RhinoCodes@users.noreply.github.com>
2020-05-16 16:52:52 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ HERE = pathlib.Path(__file__).parent
 
 # The text of the README file
 README = (HERE / "README.md").read_text()
-
+print(README)
 # This call to setup() does all the work
 setup(
     name="Ebuilder-Ultra",
